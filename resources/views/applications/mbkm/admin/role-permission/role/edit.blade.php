4212e0e909e46ca5134a355e2a61571c39033b41
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-05-14 13:59:10 +0700

    refactor: Update create and edit role card forms

--- resources/views/applications/mbkm/admin/role-permission/role/edit.blade.php
+++ resources/views/applications/mbkm/admin/role-permission/role/edit.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
-    <h1>Edit Role</h1>
+    <div class="card mb-3">
+        <div class="card-body">
+            <h1>Edit Role</h1>
 
-    <form action="{{ route('role.update', $role->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div class="col-xxl-12">
-            <div class="card mb-3">
-                <div class="card-body">
-                    <div class="mb-3">
-                        <label class="form-label">role Name</label>
-                        <input type="text" class="form-control" id="name" name="name" value="{{ $role->role }}">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Guard Name</label>
-                        <input type="text" class="form-control" id="guard_name" name="guard_name"
-                            value="{{ $role->guard_name }}">
+            <form action="{{ route('role.update', $role->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="col-xxl-12">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Role Name</label>
+                                <input type="text" class="form-control" id="name" name="name" value="{{ $role->role }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="guard_name" class="form-label">Guard Name</label>
+                                <input type="text" class="form-control" id="guard_name" name="guard_name" value="{{ $role->guard_name }}">
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <button type="submit" class="btn btn-primary">Update Role</button>
+            </form>
         </div>
-        <button type="submit" class="btn btn-primary">Update Role</button>
-    </form>
+    </div>
 </div>
-
 @endsection
