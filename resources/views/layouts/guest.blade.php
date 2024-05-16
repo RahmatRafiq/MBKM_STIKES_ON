4164da0593ea84840fb3078a39302610f51a99d4
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-05-16 20:56:19 +0700

    fix : renew login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,82 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+  <!-- Fonts -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <!-- *************
+   ************ CSS Files *************
+  ************* -->
+  <link rel="stylesheet" href="{{ asset('assets/fonts/bootstrap/bootstrap-icons.css') }}" />
+  <link rel="stylesheet" href="{{ asset('assets/css/main.min.css') }}" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+  <!-- *************
+   ************ Vendor Css Files *************
+  ************ -->
+
+  <!-- Scrollbar CSS -->
+  {{-- <link rel="stylesheet" href="{{ asset('assets/vendor/overlay-scroll/OverlayScrollbars.min.css') }}" /> --}}
+
+  <!-- Toastify CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/vendor/toastify/toastify.css') }}" />
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  {{-- <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}"> --}}
+  @stack('css')
+  @yield('css')
+</head>
+
+<body>
+  @yield('content')
+  <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/js/moment.min.js') }}"></script>
+
+  <script src="{{ asset('assets/vendor/toastify/toastify.js') }}"></script>
+
+  {{-- <script src="{{asset('assets/vendor/apex/apexcharts.min.js')}}"></script>
+      <script src="{{asset('assets/vendor/apex/custom/dash1/visitors.js')}}"></script>
+      <script src="{{asset('assets/vendor/apex/custom/dash1/sales.js')}}"></script>
+      <script src="{{asset('assets/vendor/apex/custom/dash1/sparkline.js')}}"></script>
+      <script src="{{asset('assets/vendor/apex/custom/dash1/tasks.js')}}"></script>
+      <script src="{{asset('assets/vendor/apex/custom/dash1/income.js')}}"></script> --}}
+
+  <!-- Custom JS files -->
+  {{-- <script src="{{ asset('assets/js/custom.js') }}"></script>
+  <script src="{{ asset('assets/js/todays-date.js') }}"></script> --}}
+
+  <script>
+    const sm = window.matchMedia('(min-width: 576px)');
+    const md = window.matchMedia('(min-width: 768px)');
+    const lg = window.matchMedia('(min-width: 992px)');
+    const xl = window.matchMedia('(min-width: 1200px)');
+    const xxl = window.matchMedia('(min-width: 1400px)');
+  </script>
+
+  <script>
+    @if ($errors->any())
+      @foreach ($errors->all() as $error)
+        Toastify({
+          text: "{{ $error }}",
+          duration: 5000,
+          close: true,
+          gravity: "top",
+          position: "right",
+          backgroundColor: "#f5365c",
+          stopOnFocus: true,
+        }).showToast();
+      @endforeach
+    @endif
+  </script>
+  @stack('javascript')
+  @yield('javascript')
+</body>
+
 </html>
