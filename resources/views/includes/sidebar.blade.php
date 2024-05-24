5216319e2e91cb756c1b948755167b87fdf936d2
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-05-25 02:37:24 +0800

    refactor: Update sidebar.blade.php to improve menu highlighting and add new menu items
    This commit updates the sidebar.blade.php file to improve the highlighting of the active menu item. It replaces the hard-coded 'active current-page' class with a dynamic class based on the current route. Additionally, it adds new menu items for 'Mitra (Staff)', 'Dosen Pembimbing (Staff)', and 'Lowongan (Mitra)'. These changes enhance the clarity and functionality of the sidebar menu.

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,74 +1,49 @@
-@php
-$items = [
-[
-'label' => 'Dashbord',
-'url' => route('permission.index'),
-'icon_class' => 'bi bi-box',
-],
-[
-'label' => 'Manajemen Pengguna',
-'url' => route('permission.index'),
-'icon_class' => 'bi bi-box',
-],
-
-];
-@endphp
-
-<!-- Sidebar wrapper start -->
 <nav id="sidebar" class="sidebar-wrapper">
     <!-- Sidebar menu starts -->
     <div class="sidebarMenuScroll">
         <ul class="sidebar-menu">
-            <li class="active current-page">
+            <li class="{{ request()->routeIs('dashboard') ? 'active current-page' : '' }}">
                 <a href="{{ route('dashboard') }}">
                     <i class="bi bi-box"></i>
                     <span class="menu-text">Dashboard</span>
                 </a>
             </li>
-            @foreach ($items as $item)
-            <li>
-                <a href="{{ $item['url'] }}">
-                    <i class="{{ $item['icon_class'] }}"></i>
-                    <span class="menu-text">{{ $item['label'] }}</span>
-                </a>
-            </li>
-            @endforeach
-            <li class="treeview">
-                <a href="#!">
+            <li class="treeview {{ request()->is('mbkm/admin/role-permissions*') ? 'active current-page open' : '' }}">
+                <a href="#" class="treeview-toggle">
                     <i class="bi bi-stickies"></i>
                     <span class="menu-text">Manajemen Pengguna</span>
                 </a>
-                <ul class="treeview-menu">
+                <ul class="treeview-menu" style="{{ request()->is('mbkm/admin/role-permissions*') ? 'display: block;' : 'display: none;' }}">
                     <li>
-                        <a href="{{ route('permission.index') }}">Permissions</a>
+                        <a href="{{ route('permission.index') }}" class="{{ request()->routeIs('permission.index') ? 'active-sub' : '' }}">Permissions</a>
                     </li>
                     <li>
-                        <a href="{{ route('role.index') }}">Role</a>
+                        <a href="{{ route('role.index') }}" class="{{ request()->routeIs('role.index') ? 'active-sub' : '' }}">Role</a>
                     </li>
                     <li>
-                        <a href="{{ route('user.index') }}">Users</a>
+                        <a href="{{ route('user.index') }}" class="{{ request()->routeIs('user.index') ? 'active-sub' : '' }}">Users</a>
                     </li>
                 </ul>
             </li>
-            <li>
-                <a href="{{route('profile.edit')}}">
+            <li class="{{ request()->routeIs('profile.edit') ? 'active current-page' : '' }}">
+                <a href="{{ route('profile.edit') }}">
                     <i class="bi bi-box"></i>
-                    <span class="menu-text">Profil(All User)</span>
+                    <span class="menu-text">Profil (All User)</span>
                 </a>
             </li>
-            <li>
+            <li class="{{ request()->routeIs('mitra.index') ? 'active current-page' : '' }}">
                 <a href="{{ route('mitra.index') }}">
                     <i class="bi bi-box"></i>
                     <span class="menu-text">Mitra (Staff)</span>
                 </a>
             </li>
-            <li>
+            <li class="{{ request()->routeIs('dospem.index') ? 'active current-page' : '' }}">
                 <a href="{{ route('dospem.index') }}">
                     <i class="bi bi-box"></i>
                     <span class="menu-text">Dosen Pembimbing (Staff)</span>
                 </a>
             </li>
-            <li>
+            <li class="{{ request()->routeIs('lowongan.index') ? 'active current-page' : '' }}">
                 <a href="{{ route('lowongan.index') }}">
                     <i class="bi bi-box"></i>
                     <span class="menu-text">Lowongan (Mitra)</span>
@@ -77,4 +52,4 @@ $items = [
         </ul>
     </div>
 </nav>
-<!-- Sidebar wrapper end -->
+
