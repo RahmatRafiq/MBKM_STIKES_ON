97fd25786c6ac6f25f887fc5045fb892034ef74e
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-06-04 08:12:24 +0700

    refactor: Remove unnecessary foreign key constraints in create_aktivitas_table migration
    This commit removes the unnecessary foreign key constraints in the create_aktivitas_table migration. The foreign key constraints for peserta_id, lowongan_id, mitra_id, dospem_id, laporan_harian_id, laporan_mingguan_id, and laporan_lengkap_id are commented out. These foreign key constraints are no longer needed and can be safely removed. This change simplifies the migration and improves the efficiency of the database operations.

--- database/migrations/2024_06_03_072616_create_aktivitas_table.php
+++ database/migrations/2024_06_03_072616_create_aktivitas_table.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAktifitasMbkmTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -18,13 +18,13 @@ class CreateAktifitasMbkmTable extends Migration
             $table->unsignedBigInteger('laporan_lengkap_id')->nullable();
             $table->timestamps();
 
-            $table->foreign('peserta_id')->references('id')->on('peserta')->onDelete('cascade');
-            $table->foreign('lowongan_id')->references('id')->on('lowongan')->onDelete('cascade');
-            $table->foreign('mitra_id')->references('id')->on('mitra_profile')->onDelete('cascade');
-            $table->foreign('dospem_id')->references('id')->on('dosen_pembimbing_lapangan')->onDelete('cascade');
-            $table->foreign('laporan_harian_id')->references('id')->on('laporan_harian')->onDelete('cascade');
-            $table->foreign('laporan_mingguan_id')->references('id')->on('laporan_mingguan')->onDelete('cascade');
-            $table->foreign('laporan_lengkap_id')->references('id')->on('laporan_lengkap')->onDelete('cascade');
+            // $table->foreign('peserta_id')->references('id')->on('peserta')->onDelete('cascade');
+            // $table->foreign('lowongan_id')->references('id')->on('lowongan')->onDelete('cascade');
+            // $table->foreign('mitra_id')->references('id')->on('mitra_profile')->onDelete('cascade');
+            // $table->foreign('dospem_id')->references('id')->on('dosen_pembimbing_lapangan')->onDelete('cascade');
+            // $table->foreign('laporan_harian_id')->references('id')->on('laporan_harian')->onDelete('cascade');
+            // $table->foreign('laporan_mingguan_id')->references('id')->on('laporan_mingguan')->onDelete('cascade');
+            // $table->foreign('laporan_lengkap_id')->references('id')->on('laporan_lengkap')->onDelete('cascade');
         });
     }
 
@@ -32,4 +32,4 @@ class CreateAktifitasMbkmTable extends Migration
     {
         Schema::dropIfExists('aktifitas_mbkm');
     }
-}
+};
