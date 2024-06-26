435a8f79c0fa5f25b958da592c40f82034daceee
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-06-26 13:59:35 +0700

    refactor: Update BatchMbkmController to use validated data in store, update, and destroy methods

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrasiController;
 
 
-use App\Http\Controllers\BatchMbkmController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
@@ -14,7 +13,8 @@ Route::middleware('auth')->group(function () {
     })->middleware(['auth'])->name('dashboard');
 
 
-    Route::resource('mbkm/batch-mbkms', BatchMbkmController::class);
+    Route::resource('mbkm/batch-mbkms', \App\Http\Controllers\BatchMbkmController::class);
+    Route::post('mbkm/batch-mbkms/json', [\App\Http\Controllers\BatchMbkmController::class, 'json'])->name('batch-mbkms.json');
 
     Route::get('mbkm/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('mbkm/profile', [ProfileController::class, 'update'])->name('profile.update');
