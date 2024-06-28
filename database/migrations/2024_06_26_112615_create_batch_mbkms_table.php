b32bdfe95bc9b94a14803d72370c3fbfbaed8a56
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-06-28 15:45:07 +0800

    refactor: Add is_active field to BatchMbkm table migration

--- database/migrations/2024_06_26_112615_create_batch_mbkms_table.php
+++ database/migrations/2024_06_26_112615_create_batch_mbkms_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->date('semester_start');
             $table->date('semester_end');
+            $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
     }
