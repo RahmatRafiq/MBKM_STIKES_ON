1107c2f2fa9403aa05c76dca0d33acb058ba5c92
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-06-28 15:45:19 +0800

    refactor: Update BatchMbkm create and edit views
    Update the create and edit views for BatchMbkm to include an "Active" checkbox field. This allows users to mark a batch as active or inactive. The "Active" field is added to the form and the value is stored in the database. The checkbox is disabled in the edit view to prevent modification of the active status. This change improves the user interface and provides better control over batch activation.

--- resources/views/applications/mbkm/batch-mbkm/create.blade.php
+++ resources/views/applications/mbkm/batch-mbkm/create.blade.php
@@ -1,3 +1,34 @@
+{{-- @extends('layouts.app')
+
+@section('content')
+    <div class="card mb-3">
+        <div class="card-body">
+            <div class="card-header">
+                <h5 class="card-title">Add New Batch MBKM</h5>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('batch-mbkms.store') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" required placeholder="Name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="semester_start" class="form-label">Semester Start</label>
+                        <input type="date" class="form-control" id="semester_start" name="semester_start" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="semester_end" class="form-label">Semester End</label>
+                        <input type="date" class="form-control" id="semester_end" name="semester_end" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add Batch MBKM</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection --}}
+
+
 @extends('layouts.app')
 
 @section('content')
@@ -20,6 +51,11 @@
                     <div class="mb-3">
                         <label for="semester_end" class="form-label">Semester End</label>
                         <input type="date" class="form-control" id="semester_end" name="semester_end" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="is_active" class="form-label">Active</label>
+                        <input type="hidden" name="is_active" value="0">
+                        <input type="checkbox" id="is_active" name="is_active" value="1">
                     </div>
                     <button type="submit" class="btn btn-primary">Add Batch MBKM</button>
                 </form>
