152ae3aa917b45175650ea0ce68a1a713432671d
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-06-30 20:08:24 +0800

    refactor: Update TypeProgramController to use DataTable helper for JSON response

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,22 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegistrasiController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegistrasiController;
-
-
-
-use App\Http\Controllers\AboutMbkmController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('applications/mbkm/dashboard');
     })->middleware(['auth'])->name('dashboard');
 
-
-    Route::resource('mbkm/about-mbkms', AboutMbkmController::class);
+    Route::resource('mbkm/about-mbkms', \App\Http\Controllers\AboutMbkmController::class);
     Route::resource('mbkm/batch-mbkms', \App\Http\Controllers\BatchMbkmController::class);
     Route::post('mbkm/batch-mbkms/json', [\App\Http\Controllers\BatchMbkmController::class, 'json'])->name('batch-mbkms.json');
 
+    Route::resource('mbkm/type-programs', \App\Http\Controllers\TypeProgramController::class);
+    Route::post('mbkm/type-programs/json', [\App\Http\Controllers\TypeProgramController::class, 'json'])->name('type-programs.json');
+    
     Route::get('mbkm/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('mbkm/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('mbkm/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -66,24 +64,23 @@ Route::middleware('auth')->group(function () {
     Route::put('/staff/registrasi/{id}', [RegistrasiController::class, 'update'])->name('staff.updateRegistrasi');
     Route::put('/staff/registrasi/{id}/dospem', [RegistrasiController::class, 'updateDospem'])->name('staff.updateDospem');
 
-
     // Route::middleware(['auth'])->group(function () {
     //     // Rute untuk laporan harian
     //     Route::get('/laporan-harian', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanHarian'])->name('laporan.harian');
     //     Route::get('/laporan-harian/create', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanHarian'])->name('laporan.harian.create');
     //     Route::post('/laporan-harian/store', [\App\Http\Controllers\AktivitasMbkmController::class, 'storeLaporanHarian'])->name('laporan.harian.store');
-    
+
     //     Route::get('/laporan-mingguan/create', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanMingguan'])->name('laporan.mingguan.create');
     //     Route::post('/laporan-mingguan/store', [\App\Http\Controllers\AktivitasMbkmController::class, 'storeLaporanMingguan'])->name('laporan.mingguan.store');
-    
+
     //     Route::get('/laporan-lengkap/create', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanLengkap'])->name('laporan.lengkap.create');
     //     Route::post('/laporan-lengkap/store', [\App\Http\Controllers\AktivitasMbkmController::class, 'storeLaporanLengkap'])->name('laporan.lengkap.store');
-    
+
     //     // Rute untuk validasi laporan
     //     Route::post('/laporan-harian/validate/{id}', [\App\Http\Controllers\AktivitasMbkmController::class, 'validateLaporanHarian'])->name('laporan.harian.validate');
     //     Route::post('/laporan-mingguan/validate/{id}', [\App\Http\Controllers\AktivitasMbkmController::class, 'validateLaporanMingguan'])->name('laporan.mingguan.validate');
     //     Route::post('/laporan-lengkap/validate/{id}', [\App\Http\Controllers\AktivitasMbkmController::class, 'validateLaporanLengkap'])->name('laporan.lengkap.validate');
-    
+
     //     // Rute untuk melihat daftar laporan
     //     Route::get('/laporan', [\App\Http\Controllers\AktivitasMbkmController::class, 'index'])->name('laporan.index');
     // });
@@ -93,11 +90,11 @@ Route::middleware('auth')->group(function () {
         Route::get('/laporan-harian', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanHarian'])->name('laporan.harian');
         Route::get('/laporan-harian/create', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanHarian'])->name('laporan.harian.create');
         Route::post('/laporan-harian/store', [\App\Http\Controllers\AktivitasMbkmController::class, 'storeLaporanHarian'])->name('laporan.harian.store');
-    
+
         // Rute untuk laporan mingguan
         Route::get('/laporan-mingguan/create', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanMingguan'])->name('laporan.mingguan.create');
         Route::post('/laporan-mingguan/store', [\App\Http\Controllers\AktivitasMbkmController::class, 'storeLaporanMingguan'])->name('laporan.mingguan.store');
-    
+
         // Rute untuk laporan lengkap
         Route::get('/laporan-lengkap/create', [\App\Http\Controllers\AktivitasMbkmController::class, 'createLaporanLengkap'])->name('laporan.lengkap.create');
         Route::post('/laporan-lengkap/store', [\App\Http\Controllers\AktivitasMbkmController::class, 'storeLaporanLengkap'])->name('laporan.lengkap.store');
@@ -110,11 +107,9 @@ Route::middleware('auth')->group(function () {
         // Rute untuk melihat daftar laporan
         Route::get('/laporan', [\App\Http\Controllers\AktivitasMbkmController::class, 'index'])->name('laporan.index');
     });
-    
 
 });
 require __DIR__ . '/auth.php';
-
 
 Route::resource('mahasiswa', \App\Http\Controllers\sisfo\MahasiswaController::class);
 Route::resource('dosen', \App\Http\Controllers\sisfo\DosenController::class);
