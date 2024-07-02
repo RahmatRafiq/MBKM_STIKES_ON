e3d1cf5399fe54757456dcc5681d95bb599a450c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-07-02 23:52:59 +0800

    feat: Update DashboardController to use Dashboard model
    This commit updates the DashboardController to use the Dashboard model instead of multiple individual models for retrieving data. It improves code organization and simplifies the logic for fetching counts.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,36 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\DosenPembimbingLapangan;
-use App\Models\LaporanHarian;
-use App\Models\LaporanLengkap;
-use App\Models\LaporanMingguan;
-use App\Models\Lowongan;
-use App\Models\MitraProfile;
-use App\Models\Peserta;
+use App\Models\Dashboard;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function dashboardAdmin()
     {
-        $peserta = Peserta::all();
-        $dosen = DosenPembimbingLapangan::all();
-        $mitra = MitraProfile::all();
-        $lowongan = Lowongan::all();
-        $laporanHarian = LaporanHarian::all();
-        $laporanBulanan = LaporanMingguan::all();
-        $laporanLengkap = LaporanLengkap::all();
+        $counts = Dashboard::getCounts();
 
-        return view('applications.mbkm.dashboard', compact
-            (
-                'peserta',
-                'dosen',
-                'mitra',
-                'lowongan',
-                'laporanHarian',
-                'laporanBulanan',
-                'laporanLengkap'
-            )
-        );
+        return view('applications.mbkm.admin.dashboard', $counts);
     }
 }
