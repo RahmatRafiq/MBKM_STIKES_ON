de13e6dac18af5ace8e112e8d3bfce701c34f9ef
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-07-03 01:57:10 +0800

    feat: Update DashboardController to use Dashboard model
    This commit updates the DashboardController to use the Dashboard model instead of multiple individual models for retrieving data. It improves code organization and simplifies the logic for fetching counts.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Dashboard;
+use App\Models\LaporanHarian;
 
 class DashboardController extends Controller
 {
     public function dashboardAdmin()
     {
+        // Mengambil counts dari model Dashboard dan LaporanHarian
         $counts = Dashboard::getCounts();
+        $laporanHarianCounts = Dashboard::getLaporanHarianStatusCounts();
+        // dd([
+        //     'counts' => $counts,
+        //     'laporanHarianCounts' => $laporanHarianCounts,
+        // ]);
 
-        return view('applications.mbkm.admin.dashboard', $counts);
+        // Mengirimkan data ke view 'applications.mbkm.admin.dashboard'
+        return view('applications.mbkm.admin.dashboard', [
+            'peserta' => $counts['peserta'],
+            'dosen' => $counts['dosen'],
+            'mitra' => $counts['mitra'],
+            'lowongan' => $counts['lowongan'],
+            'laporanHarian' => $counts['laporanHarian'],
+            'laporanMingguan' => $counts['laporanMingguan'],
+            'laporanLengkap' => $counts['laporanLengkap'],
+            'validasiCount' => $laporanHarianCounts['validasi'] ?? 0,
+            'pendingCount' => $laporanHarianCounts['pending'] ?? 0,
+            'revisiCount' => $laporanHarianCounts['revisi'] ?? 0,
+        ]);
     }
 }
