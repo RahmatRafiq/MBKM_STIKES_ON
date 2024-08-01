ad3717025991774f12bd2d9f3e6a9dd442cdf200
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-08-01 10:45:31 +0800

    refactor: Update dashboard layout for better responsiveness

--- resources/views/applications/mbkm/mitra/dashboard.blade.php
+++ resources/views/applications/mbkm/mitra/dashboard.blade.php
@@ -1,114 +1,111 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- App body starts -->
-<div class="app-body">
-    <!-- Row start -->
-    <div class="row gx-3">
-        <div class="col-md-4 col-sm-6 col-12">
-            <div class="card mb-3 card-custom background-gradient-1">
-                <div class="card-body">
-                    <div class="circle-shape shape-1"></div>
-                    <div class="circle-shape shape-2"></div>
-                    <div class="circle-shape shape-3"></div>
-                    <div class="mb-2">
-                        <i class="bi bi-people fs-1 text-white lh-1"></i>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <h5 class="m-0 text-white fw-normal">Peserta</h5>
-                        <h3 class="m-0 text-white">{{ $jumlahPeserta }}</h3>
-                    </div>
+
+<div class="row gx-3">
+    <div class="col-md-4 col-sm-6 col-12">
+        <div class="card mb-3 card-custom background-gradient-1">
+            <div class="card-body">
+                <div class="circle-shape shape-1"></div>
+                <div class="circle-shape shape-2"></div>
+                <div class="circle-shape shape-3"></div>
+                <div class="mb-2">
+                    <i class="bi bi-people fs-1 text-white lh-1"></i>
                 </div>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <div class="card mb-3 card-custom background-gradient-2">
-                <div class="card-body">
-                    <div class="circle-shape shape-1"></div>
-                    <div class="circle-shape shape-2"></div>
-                    <div class="circle-shape shape-3"></div>
-                    <div class="mb-2">
-                        <i class="bi bi-file-earmark-text fs-1 text-white lh-1"></i>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <h5 class="m-0 text-white fw-normal">Laporan Harian</h5>
-                        <h3 class="m-0 text-white">{{ $laporanHarian }}</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <div class="card mb-3 card-custom background-gradient-3">
-                <div class="card-body">
-                    <div class="circle-shape shape-1"></div>
-                    <div class="circle-shape shape-2"></div>
-                    <div class="circle-shape shape-3"></div>
-                    <div class="mb-2">
-                        <i class="bi bi-calendar-week fs-1 text-white lh-1"></i>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <h5 class="m-0 text-white fw-normal">Laporan Mingguan</h5>
-                        <h3 class="m-0 text-white">{{ $laporanMingguan }}</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-6">
-            <div class="card mb-3">
-                <div class="card-header">
-                    <h5 class="card-title">Laporan Harian Berdasarkan Status</h5>
-                </div>
-                <div class="card-body">
-                    <div id="donutChartHarian"></div>
-                </div>
-                <div class="grid text-center">
-                    <div class="g-col-4">
-                        <i class="bi bi-check-circle-fill text-success"></i>
-                        <h3 class="m-0 mt-1">{{ $validasiHarian }}</h3>
-                        <p class="text-secondary m-0">Tervalidasi</p>
-                    </div>
-                    <div class="g-col-4">
-                        <i class="bi bi-clock-fill text-primary"></i>
-                        <h3 class="m-0 mt-1 fw-bolder">{{ $pendingHarian }}</h3>
-                        <p class="text-secondary m-0">Pending</p>
-                    </div>
-                    <div class="g-col-4">
-                        <i class="bi bi-arrow-repeat text-danger"></i>
-                        <h3 class="m-0 mt-1">{{ $revisiHarian }}</h3>
-                        <p class="text-secondary m-0">Laporan Terevisi</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-6">
-            <div class="card mb-3">
-                <div class="card-header">
-                    <h5 class="card-title">Laporan Mingguan Berdasarkan Status</h5>
-                </div>
-                <div class="card-body">
-                    <div id="donutChartMingguan"></div>
-                </div>
-                <div class="grid text-center">
-                    <div class="g-col-4">
-                        <i class="bi bi-check-circle-fill text-success"></i>
-                        <h3 class="m-0 mt-1">{{ $validasiMingguan }}</h3>
-                        <p class="text-secondary m-0">Laporan Tervalidasi</p>
-                    </div>
-                    <div class="g-col-4">
-                        <i class="bi bi-clock-fill text-primary"></i>
-                        <h3 class="m-0 mt-1 fw-bolder">{{ $pendingMingguan }}</h3>
-                        <p class="text-secondary m-0">Laporan Pending</p>
-                    </div>
-                    <div class="g-col-4">
-                        <i class="bi bi-arrow-repeat text-danger"></i>
-                        <h3 class="m-0 mt-1">{{ $revisiMingguan }}</h3>
-                        <p class="text-secondary m-0">Laporan Terevisi</p>
-                    </div>
+                <div class="d-flex align-items-center justify-content-between">
+                    <h5 class="m-0 text-white fw-normal">Peserta</h5>
+                    <h3 class="m-0 text-white">{{ $jumlahPeserta }}</h3>
                 </div>
             </div>
         </div>
     </div>
-    {{-- <div class="col-xl-9 col-12"> --}}
+    <div class="col-md-4 col-sm-6 col-12">
+        <div class="card mb-3 card-custom background-gradient-2">
+            <div class="card-body">
+                <div class="circle-shape shape-1"></div>
+                <div class="circle-shape shape-2"></div>
+                <div class="circle-shape shape-3"></div>
+                <div class="mb-2">
+                    <i class="bi bi-file-earmark-text fs-1 text-white lh-1"></i>
+                </div>
+                <div class="d-flex align-items-center justify-content-between">
+                    <h5 class="m-0 text-white fw-normal">Laporan Harian</h5>
+                    <h3 class="m-0 text-white">{{ $laporanHarian }}</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-6 col-12">
+        <div class="card mb-3 card-custom background-gradient-3">
+            <div class="card-body">
+                <div class="circle-shape shape-1"></div>
+                <div class="circle-shape shape-2"></div>
+                <div class="circle-shape shape-3"></div>
+                <div class="mb-2">
+                    <i class="bi bi-calendar-week fs-1 text-white lh-1"></i>
+                </div>
+                <div class="d-flex align-items-center justify-content-between">
+                    <h5 class="m-0 text-white fw-normal">Laporan Mingguan</h5>
+                    <h3 class="m-0 text-white">{{ $laporanMingguan }}</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-6">
+        <div class="card mb-3">
+            <div class="card-header">
+                <h5 class="card-title">Laporan Harian Berdasarkan Status</h5>
+            </div>
+            <div class="card-body">
+                <div id="donutChartHarian"></div>
+            </div>
+            <div class="grid text-center">
+                <div class="g-col-4">
+                    <i class="bi bi-check-circle-fill text-success"></i>
+                    <h3 class="m-0 mt-1">{{ $validasiHarian }}</h3>
+                    <p class="text-secondary m-0">Tervalidasi</p>
+                </div>
+                <div class="g-col-4">
+                    <i class="bi bi-clock-fill text-primary"></i>
+                    <h3 class="m-0 mt-1 fw-bolder">{{ $pendingHarian }}</h3>
+                    <p class="text-secondary m-0">Pending</p>
+                </div>
+                <div class="g-col-4">
+                    <i class="bi bi-arrow-repeat text-danger"></i>
+                    <h3 class="m-0 mt-1">{{ $revisiHarian }}</h3>
+                    <p class="text-secondary m-0">Laporan Terevisi</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-6">
+        <div class="card mb-3">
+            <div class="card-header">
+                <h5 class="card-title">Laporan Mingguan Berdasarkan Status</h5>
+            </div>
+            <div class="card-body">
+                <div id="donutChartMingguan"></div>
+            </div>
+            <div class="grid text-center">
+                <div class="g-col-4">
+                    <i class="bi bi-check-circle-fill text-success"></i>
+                    <h3 class="m-0 mt-1">{{ $validasiMingguan }}</h3>
+                    <p class="text-secondary m-0">Laporan Tervalidasi</p>
+                </div>
+                <div class="g-col-4">
+                    <i class="bi bi-clock-fill text-primary"></i>
+                    <h3 class="m-0 mt-1 fw-bolder">{{ $pendingMingguan }}</h3>
+                    <p class="text-secondary m-0">Laporan Pending</p>
+                </div>
+                <div class="g-col-4">
+                    <i class="bi bi-arrow-repeat text-danger"></i>
+                    <h3 class="m-0 mt-1">{{ $revisiMingguan }}</h3>
+                    <p class="text-secondary m-0">Laporan Terevisi</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-12">
         <div class="card mb-3">
             <div class="card-header">
                 <h5 class="card-title">Jumlah Pendaftar Terhadap Lowongan</h5>
@@ -132,8 +129,7 @@
                 </table>
             </div>
         </div>
-    {{-- </div> --}}
-    <!-- Row end -->
+    </div>
 </div>
 <!-- App body ends -->
 @endsection
