dff7f40bb3c86f63ef2896f76a31331b89a58d77
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-08-19 16:36:34 +0800

    Refactor Matakuliah model

--- app/Models/sisfo/Matakuliah.php
+++ app/Models/sisfo/Matakuliah.php
@@ -2,26 +2,28 @@
 
 namespace App\Models\sisfo;
 
-use App\Models\Lowongan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Lowongan;
 
 class Matakuliah extends Model
 {
     use HasFactory;
 
-    protected $connection = 'mysql_second';
+    protected $connection = 'mysql_second'; // Mengambil data dari database mysql_second
     protected $table = 'mk';
 
     protected $fillable = [
-        'MKID', // ID Matakuliah
-        'Kode', // Kode
-        'Nama', // Nama
+        'MKID',
+        'KodeID',
+        'Nama',
         'SKS',
         'MKKode',
     ];
+
     public function lowongans()
     {
         return $this->belongsToMany(Lowongan::class, 'lowongan_has_matakuliah', 'matakuliah_id', 'lowongan_id');
     }
 }
+
