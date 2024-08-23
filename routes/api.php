214bc0cb51ae426a4a503bf0540a510a26bb697e
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-08-23 21:08:30 +0800

    refactor: Add mahasiswa-sisfo API endpoint to routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,7 @@ Route::name('api')->group(function () {
 // Rute baru tanpa autentikasi
 // Rute baru yang dilindungi oleh middleware API key
 Route::middleware([ApiKeyMiddleware::class])->get('/laporan-lengkap-peserta/json', [ApiController::class, 'getValidatedLaporanLengkap']);
-
-// Route fallback untuk menangani rute yang tidak ditemukan
+Route::get('/mahasiswa-sisfo/json', [ApiController::class, 'getDataMahasiswaSisfo']);
 Route::fallback(function(){
     return response()->json([
         'status' => 'error',
