b4762b5781bb409f1bd4177f0ca06562c786f527
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-04 06:26:41 +0700

    feat: Update app.blade.php to include additional React page component
    The app.blade.php file has been updated to include an additional React page component. This change allows for the dynamic rendering of different page components based on the current page. By adding the new component to the @vite directive, the React page component will be properly bundled and loaded. This enhancement improves the flexibility and extensibility of the application.
    
    Co-authored-by: RahmatRafiq

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @viteReactRefresh
-    @vite('resources/js/react.tsx')
+    @vite(['resources/js/react.tsx', "resources/js/Pages/{$page['component']}.tsx"])
     @inertiaHead
     @routes
     <script>
