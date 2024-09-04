79da92b8e0527d8bf4ebff4c70ea95446f241f0c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-04 20:53:11 +0700

    feat: Update Requirements component layout and content
    The Requirements component in `resources/js/Components/Home/Requirements.tsx` has been updated to improve its layout and content. The changes include:
    - Importing the `Link` component from `@inertiajs/react`
    - Adding a new section with additional requirements and notes
    - Styling adjustments for better readability and visual appeal
    
    These enhancements enhance the user experience by providing clear and comprehensive information about the general requirements for participating in Kampus Merdeka programs.

--- resources/js/Components/Home/Requirements.tsx
+++ resources/js/Components/Home/Requirements.tsx
@@ -1,4 +1,5 @@
-import { Card, CardBody, ScrollShadow, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react"
+import { Card, CardBody, Link, ScrollShadow, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react"
+import { Link as InertiaLink } from '@inertiajs/react'
 
 const headers = [
   '',
@@ -27,10 +28,12 @@ const cells = [
 const Requirements = () => {
   return (
     <section id="requirements" className="max-w-screen-xl w-full mx-auto p-3">
-      <Card>
-        <CardBody>
+      <Card classNames={{
+        base: 'bg-transparent',
+      }}>
+        <CardBody className="flex flex-col gap-3">
           <div className="mb-3">
-            <h1 className="font-bossa text-xl">Apa saja syarat keikutsertaan mahasiswa?</h1>
+            <h1 className="font-bossa text-xl md:text-3xl">Apa saja syarat keikutsertaan mahasiswa?</h1>
             <p>Berikut adalah <span className="font-bold">persyaratan umum</span> dari beberapa program Kampus Merdeka.</p>
           </div>
           <ScrollShadow
@@ -63,7 +66,7 @@ const Requirements = () => {
                   cells.map((row, index) => (
                     <TableRow key={index}>
                       {
-                        [indexes[index],...row].map((cell, index) => (
+                        [indexes[index], ...row].map((cell, index) => (
                           <TableCell key={index}
                             className="first:font-bold text-medium"
                           >
@@ -87,6 +90,25 @@ const Requirements = () => {
               </TableBody>
             </Table>
           </ScrollShadow>
+          <div>
+            <h3>Catatan Lain</h3>
+            <ol className="pl-8">
+              <li className="list-decimal">
+                Perguruan Tinggi harus merupakan Perguruan Tinggi Negeri atau Swasta
+                dan <span className="font-bold">berada di bawah Kementerian Pendidikan,
+                Kebudayaan, Riset, dan Teknologi</span>.
+                Cek panduannya <Link as={InertiaLink} href='#' className="font-bold">di sini</Link>.
+              </li>
+              <li className="list-decimal">
+                Mahasiswa hanya dapat mengikuti
+                <span className="font-bold">1 (satu) program per periode</span>.
+              </li>
+              <li className="list-decimal">
+                Harus berstatus <span className="font-bold">mahasiswa aktif</span>. Jika mahasiswa lulus/mengikuti yudisium sebelum
+                program berakhir maka dianggap mengundurkan diri dari program Kampus Merdeka.
+              </li>
+            </ol>
+          </div>
         </CardBody>
       </Card>
     </section>
