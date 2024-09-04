73771c40851406b9b63c7adb1493be3679f53711
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-04 07:33:00 +0700

    feat: Add ProgramNavigation component to Home page (50%)
    The ProgramNavigation component has been added to the Home page. This component displays a section with tabs for different programs, each containing a logo, description, and button links. It enhances the user experience by providing a visually appealing and informative navigation section on the Home page.

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import Onboarding from "@/Components/Onboarding"
+import ProgramNavigation from "@/Components/ProgramNavigation"
 import Guest from "@/Layouts/Guest"
 
 const Home = () => {
   console.log('asd')
   return (
-    <Guest className="min-h-svh">
+    <Guest className="min-h-svh flex flex-col gap-8">
       <Onboarding />
+      <ProgramNavigation />
     </Guest>
   )
 }
