19f3a2689229e422c366b5242693e722a40a7763
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-04 21:46:59 +0700

    feat: Update Footer component layout for responsiveness
    The Footer component in `resources/js/Components/Footer.tsx` has been updated to improve its layout for responsiveness. The changes include:
    - Adding the `sm:flex-row` class to the container div to display the copyright message and theme toggle in a row on small screens
    - This ensures that the footer section is visually appealing and functional on different screen sizes.
    
    Co-authored-by: RahmatRafiq

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="px-3">
       <Divider/>
-      <div className="flex justify-center p-3 items-center flex-col-reverse gap-3">
+      <div className="flex justify-between p-3 items-center flex-col-reverse gap-3 sm:flex-row">
         <span>&copy; 2021. All rights reserved.</span>
         <ThemeToggle />
       </div>
