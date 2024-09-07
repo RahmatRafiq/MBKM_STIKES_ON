fa3c66e05d289c21c79e15482c6a0fb49666a70c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-07 16:32:40 +0700

    Refactor HandleInertiaRequests middleware to include user information in shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
@@ -35,8 +36,20 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = $request->user();
+        $user = $user
+            ? [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'role' => $user->getRoleNames(),
+                'permissions' => $user->getPermissionsViaRoles()->pluck('name'),
+            ]
+            : null;
         return array_merge(parent::share($request), [
-            //
+            'auth' => [
+                'user' => $user,
+            ],
         ]);
     }
 }
