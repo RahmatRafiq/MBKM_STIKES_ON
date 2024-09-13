ab27c4904a616b0a8557114b2370692475da9df3
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-14 02:44:20 +0800

    Refactor mitraProfile method to return all image URLs as an array and clean up commented code

--- app/Http/Controllers/Landing/MitraProfileController.php
+++ app/Http/Controllers/Landing/MitraProfileController.php
@@ -8,9 +8,31 @@ use App\Models\MitraProfile;
 
 class MitraProfileController extends Controller
 {
+    // public function mitraProfile(MitraProfile $mitraProfile)
+    // {
+    //     // dd($mitraProfile);
+    //     return inertia('MitraProfile', [
+    //         'data' => [
+    //             'id' => $mitraProfile->id,
+    //             'name' => $mitraProfile->name,
+    //             'address' => $mitraProfile->address,
+    //             'phone' => $mitraProfile->phone,
+    //             'email' => $mitraProfile->email,
+    //             'website' => $mitraProfile->website,
+    //             'type' => $mitraProfile->type,
+    //             'description' => $mitraProfile->description,
+    //             'image_url' => $mitraProfile->getFirstMediaUrl('images'),
+    //             'lowongan' => $mitraProfile->lowongan,
+    //         ],
+    //     ]);
+    // }
     public function mitraProfile(MitraProfile $mitraProfile)
     {
-        // dd($mitraProfile);
+        // Ambil semua gambar yang terkait
+        $images = $mitraProfile->getMedia('images')->map(function ($media) {
+            return $media->getUrl(); // Dapatkan URL dari setiap gambar
+        });
+
         return inertia('MitraProfile', [
             'data' => [
                 'id' => $mitraProfile->id,
@@ -21,7 +43,7 @@ class MitraProfileController extends Controller
                 'website' => $mitraProfile->website,
                 'type' => $mitraProfile->type,
                 'description' => $mitraProfile->description,
-                'image_url' => $mitraProfile->getFirstMediaUrl('images'),
+                'image_url' => $images,  // Mengirimkan semua gambar sebagai array URL
                 'lowongan' => $mitraProfile->lowongan,
             ],
         ]);
