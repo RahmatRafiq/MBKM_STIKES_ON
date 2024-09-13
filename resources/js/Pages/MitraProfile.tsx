ce131c987315cef874bca5ec169a394839666d8b
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-14 02:57:39 +0800

    Rename MitraProfilePage component import to MitraProfile for consistency in naming

--- resources/js/Pages/MitraProfile.tsx
+++ resources/js/Pages/MitraProfile.tsx
@@ -1,4 +1,4 @@
-import MitraProfile from "@/Components/Mitra/MitraProfile"
+import MitraProfilePage from "@/Components/Mitra/MitraProfilePage"
 import Lowongan, { Mitra } from "@/types/lowongan"
 import Guest from "@/Layouts/Guest"
 
@@ -8,13 +8,13 @@ type Props = {
     data: Mitra&{lowongan: Lowongan[]}
 }
 
-const MitraProfilePage = ({data}: Props) => {
+const MitraProfile = ({data}: Props) => {
 //   console.log(data)
   return (
     <Guest className="min-h-svh flex flex-col gap-8">
-      <MitraProfile mitra={data} />
+      <MitraProfilePage mitra={data} />
     </Guest>
   )
 }
 
-export default MitraProfilePage
+export default MitraProfile
