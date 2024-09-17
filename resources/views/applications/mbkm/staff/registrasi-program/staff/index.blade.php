89e1f6104cc19805b12110407aa90f83e1333d15
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-09-17 13:28:04 +0800

    Implement DataTables for dynamic registration management and update table ID

--- resources/views/applications/mbkm/staff/registrasi-program/staff/index.blade.php
+++ resources/views/applications/mbkm/staff/registrasi-program/staff/index.blade.php
@@ -7,7 +7,7 @@
             <h5 class="card-title">Manajemen Registrasi</h5>
         </div>
         <div class="table-responsive">
-            <table class="table styled-table" id="lowongan">
+            <table class="table styled-table" id="registrations">
                 <thead>
                     <tr>
                         <th>ID Registrasi</th>
@@ -19,108 +19,86 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($registrations as $registration)
-                    <tr>
-                        <td>{{ $registration->id }}</td>
-                        <td>{{ $registration->nama_peserta }}</td>
-                        <td>{{ $registration->nama_lowongan }}</td>
-                        <td>
-                            <span class="badge 
-                                        @if($registration->status == 'registered') badge-registered
-                                        @elseif($registration->status == 'processed') badge-processed
-                                        @elseif($registration->status == 'accepted') badge-accepted
-                                        @elseif($registration->status == 'rejected') badge-rejected
-                                        @elseif($registration->status == 'rejected_by_user') badge-rejected_by_user
-                                        @elseif($registration->status == 'accepted_offer') badge-accepted_offer
-                                        @elseif($registration->status == 'placement') badge-placement
-                                        @endif">
-                                {{ $registration->status }}
-                            </span>
-                        </td>
-                        <td>
-                            @if ($registration->status == 'accepted_offer')
-                            <form action="{{ route('staff.updateDospem', $registration->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <div class="form-group">
-                                    <select name="dospem_id" class="form-control" required>
-                                        <option value="">Pilih Dosen Pembimbing</option>
-                                        @foreach ($dospems as $dospem)
-                                        <option value="{{ $dospem->id }}" {{ $registration->dospem_id == $dospem->id ?
-                                            'selected' : '' }}>
-                                            {{ $dospem->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <button type="submit" class="btn btn-success mt-2">Update</button>
-                            </form>
-                            @elseif ($registration->status == 'placement' && $registration->dospem)
-                            {{ $registration->dospem->name }}
-                            @else
-                            <span class="text-muted">-</span>
-                            @endif
-                        </td>
-                        <td>
-                            {{-- Tombol Lihat Dokumen jika status adalah 'registered' --}}
-                            @if ($registration->status == 'registered')
-                            <a href="{{ route('registrasi.documents', $registration->id) }}" class="btn btn-info">Lihat
-                                Dokumen</a>
-                            @endif
-
-                            {{-- Kondisi untuk status lain --}}
-                            @if (
-                            $registration->status != 'rejected' &&
-                            $registration->status != 'placement' &&
-                            $registration->status != 'rejected_by_user')
-                            @if ($registration->status == 'accepted_offer' && $registration->dospem_id)
-                            <form action="{{ route('staff.updateRegistrasi', $registration->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <input type="hidden" name="status" value="placement">
-                                <button type="submit" class="btn btn-success mt-2">Penempatan</button>
-                            </form>
-                            @else
-                            <form action="{{ route('staff.updateRegistrasi', $registration->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <select name="status" class="form-control">
-                                    <option value="registered" @if ($registration->status == 'registered') selected
-                                        @endif>
-                                        Terdaftar</option>
-                                    <option value="processed" @if ($registration->status == 'processed') selected
-                                        @endif>Diproses
-                                    </option>
-                                    <option value="accepted" @if ($registration->status == 'accepted') selected @endif>
-                                        Diterima
-                                    </option>
-                                    <option value="accepted_offer" @if ($registration->status == 'accepted_offer')
-                                        selected @endif>
-                                        Terima Tawaran</option>
-                                    <option value="rejected" @if ($registration->status == 'rejected') selected @endif>
-                                        Ditolak
-                                    </option>
-                                    <option value="rejected_by_user" @if ($registration->status == 'rejected_by_user')
-                                        selected @endif>Ditolak oleh
-                                        Peserta</option>
-
-                                </select>
-                                <button type="submit" class="btn btn-success mt-2">Update</button>
-                            </form>
-                            @endif
-                            @else
-                            <span class="text-muted">{{ $registration->status }}</span>
-                            @endif
-                        </td>
-                    </tr>
-                    @endforeach
+                    {{-- Data akan diisi secara dinamis oleh DataTables --}}
                 </tbody>
             </table>
         </div>
-        <a href="#" class="show-all-link">Show All</a>
     </div>
 </div>
 @endsection
 
 @push('css')
-<link rel="stylesheet" href="{{ asset('assets/css/badges.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/DataTables/datatables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/badges.css') }}">
+@endpush
+
+@push('javascript')
+    <script src="{{ asset('assets/DataTables/datatables.min.js') }}"></script>
+    <script src="{{ asset('assets/js/sweetalert2.all.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $('#registrations').DataTable({
+                responsive: true,
+                serverSide: true,
+                processing: true,
+                paging: true,
+                ajax: {
+                    url: '{{ route('registrasi.json') }}', // Pastikan route json benar
+                    type: 'POST',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                },
+                columns: [
+                    {
+                        data: 'id',
+                    },
+                    {
+                        data: 'nama_peserta',
+                    },
+                    {
+                        data: 'nama_lowongan',
+                    },
+                    {
+                        data: 'status',
+                        render: function(data, type, row) {
+                            return `<span class="badge badge-${data.replace('_', '-')}">${data}</span>`;
+                        }
+                    },
+                    {
+                        data: 'dospem.name',
+                        defaultContent: '<span class="text-muted">-</span>' // Jika tidak ada dospem
+                    },
+                    {
+                        data: 'action',
+                        orderable: false,
+                        searchable: false,
+                        render: function(data, type, row) {
+                            let html = '';
+                            if (row.status == 'registered') {
+                                html += `<a href="{{ route('registrasi.documents', ':id') }}" class="btn btn-info">Lihat Dokumen</a>`.replace(':id', row.id);
+                            }
+
+                            if (row.status != 'rejected' && row.status != 'placement' && row.status != 'rejected_by_user') {
+                                html += `<form action="{{ route('staff.updateRegistrasi', ':id') }}" method="POST" style="display: inline;">`.replace(':id', row.id);
+                                html += '@csrf @method("PUT")';
+                                html += `<select name="status" class="form-control">`;
+                                html += `<option value="registered" ${(row.status == 'registered') ? 'selected' : ''}>Terdaftar</option>`;
+                                html += `<option value="processed" ${(row.status == 'processed') ? 'selected' : ''}>Diproses</option>`;
+                                html += `<option value="accepted" ${(row.status == 'accepted') ? 'selected' : ''}>Diterima</option>`;
+                                html += `<option value="accepted_offer" ${(row.status == 'accepted_offer') ? 'selected' : ''}>Terima Tawaran</option>`;
+                                html += `<option value="rejected" ${(row.status == 'rejected') ? 'selected' : ''}>Ditolak</option>`;
+                                html += `<option value="rejected_by_user" ${(row.status == 'rejected_by_user') ? 'selected' : ''}>Ditolak oleh Peserta</option>`;
+                                html += `</select>`;
+                                html += `<button type="submit" class="btn btn-success mt-2">Update</button>`;
+                                html += `</form>`;
+                            }
+
+                            return html;
+                        }
+                    }
+                ]
+            });
+        });
+    </script>
 @endpush
